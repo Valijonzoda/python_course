49dc741fc57194bdaa1f271618b608d1c01f66f0
Valijonzoda <valijonzodafirdavs@gmail.com>
2023-01-19 17:07:25 +0500

    update

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ def printText():
 printText()
 printText()
 printText()
+
+def sqRing(p):
+    s = 3.1415 * (p ** 2)
+    return s
+print(sqRing(20))
+
+
+def getSqr(w, h):
+    return 2*(w+h)
+print(getSqr(4, 5))

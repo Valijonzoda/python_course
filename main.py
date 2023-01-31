5694c8ddc070416618c632b13e637dbfef8b0b39
Valijonzoda <valijonzodafirdavs@gmail.com>
2023-01-31 11:50:48 +0500

    *args and **kwargs

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ print(sqRing(20))
 def getSqr(w, h):
     return 2*(w+h)
 print(getSqr(4, 5)) #up data
+
+
+def get_args(a, b, c, d):
+    print(a, b, c, d)
+
+l = ("hello", 77, [1, "hh", "32"], False)
+get_args(*l)
+
+
+def aaargs(*args, **kwargs):
+    print(args)
+    print(kwargs)
+print(aaargs(1,2,2,2,2,2,2,23, b = 4, l = 8))
